faf7ff0a317b67002ccd6d718f261ef63286bb59
Lucas Godoy <lcsdegodoy@outlook.com>
2020-09-03 21:31:08 -0300

    feat: add home template

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,51 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="home bg-contact100">
+    <div class="container-contact100">
+      <div class="wrap-contact100">
+        <div class="contact100-pic">
+          <img src="~@/assets/images/img-01.png" alt="IMG">
+        </div>
+
+        <form class="contact100-form validate-form">
+          <span class="contact100-form-title">
+            Enviar email
+          </span>
+
+          <div class="wrap-input100 validate-input">
+            <input class="input100" type="text" name="name" placeholder="Nome">
+            <span class="focus-input100"></span>
+            <span class="symbol-input100">
+              <i class="fa fa-user" aria-hidden="true"></i>
+            </span>
+          </div>
+
+          <div class="wrap-input100 validate-input">
+            <input class="input100" type="text" name="email" placeholder="Email">
+            <span class="focus-input100"></span>
+            <span class="symbol-input100">
+              <i class="fa fa-envelope" aria-hidden="true"></i>
+            </span>
+          </div>
+
+          <div class="wrap-input100 validate-input">
+            <textarea class="input100" name="message" placeholder="Message"></textarea>
+            <span class="focus-input100"></span>
+          </div>
+
+          <div class="container-contact100-form-btn">
+            <button class="contact100-form-btn">
+              Enviar
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 
-@Options({
-  components: {
-    HelloWorld
-  }
-})
+@Options({})
 export default class Home extends Vue {}
 </script>
