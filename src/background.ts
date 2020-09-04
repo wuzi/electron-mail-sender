93f31716efd13883978cc96f57490e8ed792cdfc
Lucas Godoy <lcsdegodoy@outlook.com>
2020-09-03 22:16:55 -0300

    feat: set nodeIntegration to always true

--- src/background.ts
+++ src/background.ts
@@ -22,8 +22,7 @@ function createWindow () {
     webPreferences: {
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
-      nodeIntegration: (process.env
-        .ELECTRON_NODE_INTEGRATION as unknown) as boolean
+      nodeIntegration: true
     }
   })
 
